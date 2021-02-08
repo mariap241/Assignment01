72d5992f8f238d1cf3032718afcd10a5c3220aed
Maria Palacios <mariapalacios5889@gmail.com>
2021-02-08 14:09:40 -0600

    problem #2 code

--- Assignment01/src/main/java/edu/ru/assignment01/MainClass.java
+++ Assignment01/src/main/java/edu/ru/assignment01/MainClass.java
@@ -1,31 +1,48 @@
-
 package edu.ru.assignment01;
 
 public class MainClass {
-    
-    
-    public static void main(String [] args){
+
+    public static void main(String[] args) {
         //create and initialize array1
         String[] array1 = {"apples", "snow", "kitten", "cars"};
-        
+
         //create array2
         String array2[] = new String[4];
-        
+
         //copy data from array1 to array2
-        for (int index=0; index< array1.length; index++){
-            array2[index]=array1[index];
+        for (int index = 0; index < array1.length; index++) {
+            array2[index] = array1[index];
         }
         //add "_" prefix to array1
-        for (int index=0; index< array1.length; index++){
-            array1[index]="_"+ array1[index];
+        for (int index = 0; index < array1.length; index++) {
+            array1[index] = "_" + array1[index];
         }
         //print array1
-        for (String element: array1){
+        for (String element : array1) {
             System.out.println(element);
         }
         //print array2
-        for (String element: array2){
+        for (String element : array2) {
             System.out.println(element);
         }
     }
+
+    public static int[] removeElement(int[] inputArray, int n) {
+        int[] arr1 = new int[inputArray.length - 1];
+
+        for (int i = 0; i < inputArray.length; i++) {
+            if (inputArray[i] == n) {
+                int badVariable = i;
+
+                if (inputArray[i] == badVariable) {
+                    continue;
+                }
+                arr1[i] = inputArray[i];
+
+            }
+ 
+        }
+        return arr1;
+    }
 }
+        
