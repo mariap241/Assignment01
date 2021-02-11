0e25eb06f316453230ea38bd3cc6bfb2e522c5fb
Maria Palacios <mariapalacios5889@gmail.com>
2021-02-11 16:24:07 -0600

    code for problem #4

--- Assignment01/src/main/java/edu/ru/assignment01/MainClass.java
+++ Assignment01/src/main/java/edu/ru/assignment01/MainClass.java
@@ -1,53 +1,76 @@
 package edu.ru.assignment01;
 
 public class MainClass {
-
-    public static void main(String[] args) {
-
+    
+    
+    public static void main(String[] args){
+        
         int[] array1 = {45, 21, 22, 31, 65, 12, 56, 19, 100};
-
-        int count = 0;
-        for (int index = 0; index < array1.length; index++) {
-            if (array1[index] % 2 == 0) {
-                count = count + 1;
-            }
+        
+        
+        int highestElement=highestElement(array1);
+        
+        System.out.println("The highest element is "+ highestElement);
+    }
+    public static int highestElement(int[] inputArray){
+        
+        int highestElement=inputArray[0];
+        
+        for(int index=0; index<inputArray.length; index++){
+            if (inputArray[index]> highestElement)
+                highestElement=inputArray[index];
+           
         }
-        int[] array2 = new int[count];
-        array2 = dropOddNumbers(array1);
-
-        for (int element : array2) {
-            System.out.println(element);
-        }
+        return highestElement;
+        
     }
 
-    public static int[] dropOddNumbers(int[] arr) {
-        //count the number of even numbers, this'll be the new array length
-        //count number of even numbers
+//    public static void main(String[] args) {
+//
+//        int[] array1 = {45, 21, 22, 31, 65, 12, 56, 19, 100};
+//
+//        int count = 0;
+//        for (int index = 0; index < array1.length; index++) {
+//            if (array1[index] % 2 == 0) {
+//                count = count + 1;
+//            }
+//        }
+//        int[] array2 = new int[count];
+//        array2 = dropOddNumbers(array1);
+//
+//        for (int element : array2) {
+//            System.out.println(element);
+//        }
+//    }
 
-        int count = 0;
-        for (int index = 0; index < arr.length; index++) {
-            if (arr[index] % 2 == 0) {
-                count = count + 1;
-            }
-        }
-
-        //allocate memory as big as even number count 
-        int[] resultArray = new int[count];
-        int resultArrayIndex = 0;
-
-        //copy even numbers over to new array
-        for (int index = 0; index < arr.length; index++) {
-            if (arr[index] % 2 != 0) {
-                continue;
-            }
-
-            resultArray[resultArrayIndex] = arr[index];
-            resultArrayIndex++;
-
-        }
-        return resultArray;
-
-    }
+//    public static int[] dropOddNumbers(int[] arr) {
+//        //count the number of even numbers, this'll be the new array length
+//        //count number of even numbers
+//
+//        int count = 0;
+//        for (int index = 0; index < arr.length; index++) {
+//            if (arr[index] % 2 == 0) {
+//                count = count + 1;
+//            }
+//        }
+//
+//        //allocate memory as big as even number count 
+//        int[] resultArray = new int[count];
+//        int resultArrayIndex = 0;
+//
+//        //copy even numbers over to new array
+//        for (int index = 0; index < arr.length; index++) {
+//            if (arr[index] % 2 != 0) {
+//                continue;
+//            }
+//
+//            resultArray[resultArrayIndex] = arr[index];
+//            resultArrayIndex++;
+//
+//        }
+//        return resultArray;
+//
+//    }
 }
 
 //    public static void main(String[] args) {
