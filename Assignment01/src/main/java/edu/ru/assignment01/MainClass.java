c92c47101082417181dcdce67110e9466fbcdddb
Maria Palacios <mariapalacios5889@gmail.com>
2021-02-11 16:03:10 -0600

    code for problem #3

--- Assignment01/src/main/java/edu/ru/assignment01/MainClass.java
+++ Assignment01/src/main/java/edu/ru/assignment01/MainClass.java
@@ -3,46 +3,93 @@ package edu.ru.assignment01;
 public class MainClass {
 
     public static void main(String[] args) {
-        //create and initialize array1
-        String[] array1 = {"apples", "snow", "kitten", "cars"};
 
-        //create array2
-        String array2[] = new String[4];
+        int[] array1 = {45, 21, 22, 31, 65, 12, 56, 19, 100};
 
-        //copy data from array1 to array2
+        int count = 0;
         for (int index = 0; index < array1.length; index++) {
-            array2[index] = array1[index];
+            if (array1[index] % 2 == 0) {
+                count = count + 1;
+            }
         }
-        //add "_" prefix to array1
-        for (int index = 0; index < array1.length; index++) {
-            array1[index] = "_" + array1[index];
-        }
-        //print array1
-        for (String element : array1) {
-            System.out.println(element);
-        }
-        //print array2
-        for (String element : array2) {
+        int[] array2 = new int[count];
+        array2 = dropOddNumbers(array1);
+
+        for (int element : array2) {
             System.out.println(element);
         }
     }
 
-    public static int[] removeElement(int[] inputArray, int n) {
-        int[] arr1 = new int[inputArray.length - 1];
+    public static int[] dropOddNumbers(int[] arr) {
+        //count the number of even numbers, this'll be the new array length
+        //count number of even numbers
 
-        for (int i = 0; i < inputArray.length; i++) {
-            if (inputArray[i] == n) {
-                int badVariable = i;
-
-                if (inputArray[i] == badVariable) {
-                    continue;
-                }
-                arr1[i] = inputArray[i];
-
+        int count = 0;
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index] % 2 == 0) {
+                count = count + 1;
             }
- 
         }
-        return arr1;
+
+        //allocate memory as big as even number count 
+        int[] resultArray = new int[count];
+        int resultArrayIndex = 0;
+
+        //copy even numbers over to new array
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index] % 2 != 0) {
+                continue;
+            }
+
+            resultArray[resultArrayIndex] = arr[index];
+            resultArrayIndex++;
+
+        }
+        return resultArray;
+
     }
 }
-        
+
+//    public static void main(String[] args) {
+//        //create and initialize array1
+//        String[] array1 = {"apples", "snow", "kitten", "cars"};
+//
+//        //create array2
+//        String array2[] = new String[4];
+//
+//        //copy data from array1 to array2
+//        for (int index = 0; index < array1.length; index++) {
+//            array2[index] = array1[index];
+//        }
+//        //add "_" prefix to array1
+//        for (int index = 0; index < array1.length; index++) {
+//            array1[index] = "_" + array1[index];
+//        }
+//        //print array1
+//        for (String element : array1) {
+//            System.out.println(element);
+//        }
+//        //print array2
+//        for (String element : array2) {
+//            System.out.println(element);
+//        }
+//    }
+//
+//    public static int[] removeElement(int[] inputArray, int n) {
+//        int[] arr1 = new int[inputArray.length - 1];
+//
+//        for (int i = 0; i < inputArray.length; i++) {
+//            if (inputArray[i] == n) {
+//                int badVariable = i;
+//
+//                if (inputArray[i] == badVariable) {
+//                    continue;
+//                }
+//                arr1[i] = inputArray[i];
+//
+//            }
+// 
+//        }
+//        return arr1;
+//    } 
+
